ed2ffa798329e42228b73a6dace91c7256ba0405
Aagat Sapkota <ars9@williams.edu>
2020-07-10 00:25:16 -0400

    Final

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
  
     <h1>
     {JSONData.title}
-    {JSONData.content.map((data, index) => {
+    {JSONData.content.map((data, index) => {   //The same data is in another class as well, was just using it here to make it all available in one place
         return <li key={`content_item_${index}`}>{data.item}</li>
       })}
   </h1> </div>
